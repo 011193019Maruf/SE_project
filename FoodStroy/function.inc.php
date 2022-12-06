e1a6f67e484d5d632e261b8f276bcd0baeeefb98
mim <fhossain193004@bscse.uiu.ac.bd>
2022-12-06 10:34:32 +0600

    foodstory update 1

---
+++ FoodStroy/function.inc.php
@@ -0,0 +1,37 @@
+<?php 
+function pr($arr){
+	echo '<pre>';
+	print_r($arr);
+
+}
+function prx($arr){
+	echo '<pre>';
+print_r($arr);
+die();
+}
+
+function get_safe_value($con, $str){
+	if($str!=''){
+		$str=trim($str);
+	return mysqli_real_escape_string($con,$str);
+}
+}
+
+function get_product($con,$type='', $limit=''){
+	$sql="SELECT * FROM food_product where status=1";
+$res=mysqli_query($con,$sql);
+	$data=array();
+	while($row=mysqli_fetch_assoc($res)){
+		$data[]=$row;
+	}
+	if($type=='offer'){
+		$sql.="ORDER BY ID desc";
+	}
+	if($limit!=''){
+		$sql.="limit $limit";
+	}
+
+	return $data;
+}
+ ?>
+
