5a793028ca9af985c5190e8d6fe5ecd744cea426
[Maruf Ur Rahman] <[maruf.ur.rahman.0009@gmail.com]>
2022-12-22 20:36:38 +0600

    database for inventory version 1.0

---
+++ inventory.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 22, 2022 at 03:34 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inventory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `cid` int(11) NOT NULL,
+  `parent_cat` int(11) NOT NULL,
+  `category_name` varchar(255) NOT NULL,
+  `status` enum('1','0') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`cid`, `parent_cat`, `category_name`, `status`) VALUES
+(1, 0, 'Equipment', '1'),
+(2, 0, 'Utensils', '1'),
+(3, 0, 'Ingredients', '1'),
+(4, 0, 'Dishes', '1'),
+(5, 0, 'Cleaning supplies', '1'),
+(6, 0, 'Packaging materials', '1'),
+(7, 0, 'Small appliances', '1'),
+(8, 0, 'Cookware', '1'),
+(9, 0, 'Bakeware', '1'),
+(10, 0, 'Bar equipment', '1'),
+(11, 0, 'Storage containers', '1'),
+(12, 0, 'Table service', '1'),
+(13, 0, 'Menus', '1'),
+(14, 0, 'Point of sale (POS) system', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `usertype` enum('Admin','Other') NOT NULL,
+  `register_date` date NOT NULL,
+  `last_login` datetime NOT NULL,
+  `notes` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `usertype`, `register_date`, `last_login`, `notes`) VALUES
+(8, 'x', 'x@gmail.com', '$2y$08$TjuX9jSt9WOLIeZuSjsz0OoHg.rkt2Pq7kI5WiMLPU.Dj.mUOVReK', 'Admin', '2022-12-21', '2022-12-22 02:12:02', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`cid`),
+  ADD UNIQUE KEY `category_name` (`category_name`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
